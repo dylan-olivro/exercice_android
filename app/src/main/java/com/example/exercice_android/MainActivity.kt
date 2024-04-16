68d18ee40118b587e353120fa1f6c3e9c7def3f5
Dylan Olivro <dylanolivro94@gmail.com>
2024-04-16 12:31:49 +0200

    update verif

--- app/src/main/java/com/example/exercice_android/MainActivity.kt
+++ app/src/main/java/com/example/exercice_android/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,29 +28,17 @@ class MainActivity : AppCompatActivity() {
             val companyEditText = findViewById<EditText>(R.id.company)
             val fullCompany = companyEditText.text.toString()
 
-            val intent = Intent(this, SecondActivity::class.java)
-            intent.action = Intent.ACTION_VIEW
-            intent.addCategory("user")
-            intent.putExtra("name", fullName)
-            intent.putExtra("job", fullJob)
-            intent.putExtra("company", fullCompany)
-            startActivity(intent)
+            if(fullName.isEmpty() || fullJob.isEmpty() || fullCompany.isEmpty()) {
+                Toast.makeText(this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.action = Intent.ACTION_VIEW
+                intent.addCategory("user")
+                intent.putExtra("name", fullName)
+                intent.putExtra("job", fullJob)
+                intent.putExtra("company", fullCompany)
+                startActivity(intent)
+            }
         }
-    }
-
-    fun submitbuttonHandler(view: View?) {
-        //Decide what happens when the user clicks the submit button
-        val nameEditText = findViewById<EditText>(R.id.names)
-        val fullName = nameEditText.text.toString()
-
-        val jobEditText = findViewById<EditText>(R.id.job)
-        val fullJob = jobEditText.text.toString()
-
-        val companyEditText = findViewById<EditText>(R.id.company)
-        val fullCompany = companyEditText.text.toString()
-
-        Log.i(TAG, fullName)
-        Log.i(TAG, fullJob)
-        Log.i(TAG, fullCompany)
     }
 }
